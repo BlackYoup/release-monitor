742406bf289036c1bdba0f2755985a0c107a30c4
BlackYoup <a.lefebvre@outlook.fr>
2016-10-18 19:54:31 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -26,56 +26,14 @@ fn main() {
     if args.len() <= 1 {
         println!("Use: release-monitor <projects file>");
     } else {
-        let projects: Vec<Box<TProject>> = parse_projects(&args[1]);
-
-        for project in projects {
-            let mut project: Project = project.to_project();
-            let saved_project = project.get_saved_project(&config);
-
-            if saved_project.is_some() {
-                // TODO: what happens if there are no versions yet ?
-                let _saved_project = saved_project.unwrap();
-                if Project::has_new_version(&_saved_project, &project) {
-                    println!("Project {} is more recent", project.name);
-                } else {
-                    println!("Project {} is the same", project.name);
-                }
-
-                let object_id = _saved_project.object_id.unwrap();
-
-                project.set_object_id(object_id);
-            } else {
-                println!("Project {} not yet in database", project.name);
-            }
-
-            match project.save(&config) {
-                true => println!("Project {} updated", project.name),
-                false => println!("Couldn't update project {}", project.name)
-            }
+        match &args[1] {
+            "add-project" => add_project(&args[1]),
+            "watch" => watch()
         }
     }
 }
 
-// TODO
-#[allow(unused_must_use)]
-fn parse_projects(file_path: &String) -> Vec<Box<TProject>>{
-    let mut file = File::open(file_path).unwrap();
-    let mut buffer = String::new();
-    let mut ret: Vec<Box<TProject>> = Vec::new();
-
-    file.read_to_string(&mut buffer);
-
-    let lines = buffer.split('\n');
-
-    for url in lines{
-        let p = match_project(url.to_string());
-
-        if p.is_some() {
-            ret.push(p.unwrap());
-        }
-    }
-
-    return ret;
+fn add_project(url: &String) {
 }
 
 fn match_project(url: String) -> Option<Box<TProject>>{
@@ -87,5 +45,35 @@ fn match_project(url: String) -> Option<Box<TProject>>{
         return Some(Box::new(Github::new(url)));
     } else{
         return None;
+    }
+}
+
+fn watch() {
+    let projects: Vec<Project> = Project::get_all_projects();
+
+    for project in projects {
+        let mut project: Project = project.to_project();
+        let saved_project = project.get_saved_project(&config);
+
+        if saved_project.is_some() {
+            // TODO: what happens if there are no versions yet ?
+            let _saved_project = saved_project.unwrap();
+            if Project::has_new_version(&_saved_project, &project) {
+                println!("Project {} is more recent", project.name);
+            } else {
+                println!("Project {} is the same", project.name);
+            }
+
+            let object_id = _saved_project.object_id.unwrap();
+
+            project.set_object_id(object_id);
+        } else {
+            println!("Project {} not yet in database", project.name);
+        }
+
+        match project.save(&config) {
+            true => println!("Project {} updated", project.name),
+            false => println!("Couldn't update project {}", project.name)
+        }
     }
 }
